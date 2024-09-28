60bdb1ba591b50de7fe5957c8e393fa20a1503e2
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2024-09-28 21:31:14 +0300

    added search field in dashboard

--- scribe_ui/src/app/dashboard/page.tsx
+++ scribe_ui/src/app/dashboard/page.tsx
@@ -4,17 +4,42 @@ import {Box} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import IconButton from '@mui/material/IconButton';
 import AddIcon from '@mui/icons-material/Add';
+import SearchIcon from '@mui/icons-material/Search';
+import TextField from '@mui/material/TextField';
+import {InputAdornment} from "@mui/material";
+
 
 export default function Page() {
     return (
         <Box padding={1}>
             <Toolbar>
                 <Typography variant={'h6'}>Chats</Typography>
-                <IconButton
+
+                {/* Search Bar*/}
+                <TextField
                     sx={{marginLeft: 'auto'}}
-                >
+                    variant={'standard'}
+                    label={"Search..."}
+                    slotProps = {
+                        {
+                            input: {
+                                endAdornment: (
+                                    <InputAdornment position={'end'}>
+                                        <IconButton>
+                                            <SearchIcon/>
+                                        </IconButton>
+                                    </InputAdornment>
+                                )
+                            }
+                        }
+                    }
+                />
+
+                <Divider orientation={'vertical'}/>
+                <IconButton>
                     <AddIcon/>
                 </IconButton>
+
             </Toolbar>
             <Divider/>
         </Box>
