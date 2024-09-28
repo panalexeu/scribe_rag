6c091827436bbd8009e0a4320e1935148517cbbe
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2024-09-28 21:31:47 +0300

    minor refactoring

--- scribe_ui/src/app/page.tsx
+++ scribe_ui/src/app/page.tsx
@@ -1,7 +1,3 @@
-import AppBar from '@mui/material/AppBar';
-import Typography from '@mui/material/Typography';
-import {Toolbar} from "@mui/material";
-
 export default function Page() {
     return (
         <p>Scribe</p>
