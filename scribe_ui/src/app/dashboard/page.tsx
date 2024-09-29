49def7e54c1530213a9e01dfb144391992c9494d
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2024-09-29 21:29:30 +0300

    minor refactoring

--- scribe_ui/src/app/dashboard/page.tsx
+++ scribe_ui/src/app/dashboard/page.tsx
@@ -29,7 +29,7 @@ export default function Page() {
                 {/* Name */}
                 <Typography
                     variant={'h6'}
-                    paddingRight={1}
+                    marginRight={2}
                 >
                     Chats
                 </Typography>
