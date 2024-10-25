f920a16c1bddf3d5a83e40812532d50577542f6d
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2024-10-25 14:13:42 +0300

    cleared doc attempts

--- scribe/src/api/app.py
+++ scribe/src/api/app.py
@@ -1,19 +1,8 @@
 from fastapi import FastAPI
 
-from .routers import (
-    instruct,
-    chat
-)
-
-from src.bootstrap import wire_dependencies
-
 
 def app_factory():
     app_ = FastAPI()
-    app_.include_router(instruct.router)
-    app_.include_router(chat.router)
-
-    wire_dependencies()
 
     return app_
 
