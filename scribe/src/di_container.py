c7f39b428765c0fb2c0deb6abd7bf59ba30f0d66
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2024-10-27 17:43:51 +0200

    updated AppStartQueryHandler with considerations to new log_dir setup functionality

--- scribe/src/di_container.py
+++ scribe/src/di_container.py
@@ -2,7 +2,7 @@ from mediatr import Mediator
 from dependency_injector.containers import DeclarativeContainer
 from dependency_injector.providers import Singleton, Callable
 
-from src.system.dir import get_scribe_dir_path, get_scribe_key_file
+from src.system.dir import get_scribe_dir_path, get_scribe_key_file, get_scribe_log_dir_path
 
 
 class Container(DeclarativeContainer):
@@ -11,6 +11,9 @@ class Container(DeclarativeContainer):
     )
     scribe_key_file = Callable(
         get_scribe_key_file
+    )
+    log_dir = Callable(
+        get_scribe_log_dir_path
     )
 
     mediatr = Singleton(
--- scribe/src/handlers/app_start.py
+++ scribe/src/handlers/app_start.py
@@ -16,11 +16,13 @@ class AppStartQueryHandler:
     def __init__(
             self,
             scribe_dir: str = Provide['scribe_dir'],
-            scribe_key_file: str = Provide['scribe_key_file']
+            scribe_key_file: str = Provide['scribe_key_file'],
+            log_dir: str = Provide['log_dir']
     ):
         self.scribe_dir = scribe_dir
         self.scribe_key_file = scribe_key_file
+        self.log_dir = log_dir
 
     def handle(self, request: AppStartQuery):
         logging.info('APP STARTUP COMPLETE')
-        setup_scribe_dir(self.scribe_dir, self.scribe_key_file)
+        setup_scribe_dir(self.scribe_dir, self.scribe_key_file, self.log_dir)
