2c309c151ec1a42c981a2b7b161205631fd81050
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2024-10-29 21:44:33 +0200

    added test_api_key_orm_mapping test

--- scribe/src/domain/models.py
+++ scribe/src/domain/models.py
@@ -3,3 +3,6 @@ class ApiKeyCredential:
     def __init__(self, name: str, api_key: str):
         self.name = name
         self.api_key = api_key
+
+    def __repr__(self) -> str:
+        return f'ApiKeyCredential(name={self.name}, api_key={self.api_key})'
--- scribe/tests/unit/test_api_key_credential_model.py
+++ scribe/tests/unit/test_api_key_credential_model.py
@@ -4,6 +4,7 @@ import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from cryptography.fernet import Fernet
+from rich import print
 
 from src.domain.models import ApiKeyCredential
 from src.domain.services import encode_api_key_credential
@@ -12,7 +13,7 @@ from src.adapters.orm_models import map_orm_models
 from src.di_container import Container
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='function')
 def setup_orm_session():
     container = Container()
     engine = create_engine('sqlite:///:memory:', echo=True)
@@ -35,4 +36,25 @@ def test_encode_api_key_credential():
 
 def test_api_key_orm_mapping(setup_orm_session):
     with setup_orm_session as session:
-        pass
+        open_ai = ApiKeyCredential(
+            'open-ai',
+            '12345'
+        )
+        cohere = ApiKeyCredential(
+            'cohere',
+            '123415'
+        )
+
+        session.add_all([open_ai, cohere])
+        session.commit()
+
+    with setup_orm_session as session:
+        new_openai: ApiKeyCredential = session.get(ApiKeyCredential, 1)
+        new_cohere: ApiKeyCredential = session.get(ApiKeyCredential, 2)
+        print(new_openai, new_cohere)
+
+        # ApiKeyCredential objects are successfully retrieved from the db
+        assert new_openai.name == 'open-ai'
+        assert new_cohere.name == 'cohere'
+        assert new_cohere.api_key == '123415'
+        assert new_openai.api_key == '12345'
