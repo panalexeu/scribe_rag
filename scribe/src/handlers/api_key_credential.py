85a4f15c36037feecf73364b14a5f3b4e5c647af
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2024-10-30 20:18:09 +0200

    added api key add handler

---
+++ scribe/src/handlers/api_key_credential.py
@@ -0,0 +1,28 @@
+from dependency_injector.wiring import inject, Provide
+from mediatr import Mediator, GenericQuery
+
+from src.domain.models import ApiKeyCredential
+from src.adapters.repository import AbstractRepository
+from src.adapters.codecs import AbstractCodec
+
+
+class ApiKeyAddCommand(GenericQuery[ApiKeyCredential]):
+    name: str
+    api_key: str
+
+
+@Mediator.handler
+class ApiKeyCredAddHandler:
+
+    @inject
+    def __init__(
+            self,
+            repository: AbstractRepository[ApiKeyCredential] = Provide['api_key_cred_repo'],
+            codec: AbstractCodec = Provide['codec']
+    ):
+        self.repository = repository
+        self.codec = codec
+
+    def handle(self, request: ApiKeyAddCommand):
+        api_key_cred = ApiKeyCredential(**request.__dict__)
+        self.repository.add(api_key_cred)
