2488f8dc07c20e4fcd4dc5fd6a956b7c256a64d6
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2024-11-11 15:18:28 +0200

    changed id to int in api_key_credential_id type hint, chat model add command

--- scribe/src/handlers/chat_model.py
+++ scribe/src/handlers/chat_model.py
@@ -12,7 +12,7 @@ from src.adapters.uow import AbstractUoW
 
 class ChatModelAddCommand(BaseModel, GenericQuery[ChatModel]):
     name: ChatModelName
-    api_key_credential: id
+    api_key_credential: int
     temperature: float | None
     top_p: float | None
     base_url: str | None
