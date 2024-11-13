e6f23cec370f12e1b7b4f51fc6965a1f64449984
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2024-11-13 14:16:55 +0200

    working on create doc endpoint and handler

--- scribe/src/api/routers/document.py
+++ scribe/src/api/routers/document.py
@@ -1,6 +1,12 @@
-from fastapi import APIRouter
+from mediatr import Mediator
+from fastapi import APIRouter, Depends, UploadFile
 from dependency_injector.wiring import inject, Provide
 from pydantic import BaseModel
+
+from src.di_container import Container
+from src.handlers.document import (
+    DocAddModel
+)
 
 router = APIRouter(
     prefix='/doc',
@@ -9,12 +15,22 @@ router = APIRouter(
 
 
 class DocPostModel(BaseModel):
-    ...
+    vec_col_name: str
+    doc_processing_cnf_id: int
 
 
 @router.post(
     path='/',
 )
 @inject
-async def create_doc():
-    pass
+async def create_doc(
+        files: list[UploadFile],
+        item: DocPostModel,
+        mediatr: Mediator = Depends(Provide[Container.mediatr])
+):
+    command = DocAddModel(
+        **item.model_dump(),
+        files={file.filename: await file.read() for file in files}
+    )
+
+    return await mediatr.send_async(command)
---
+++ scribe/src/handlers/document.py
@@ -0,0 +1,35 @@
+from dependency_injector.wiring import inject, Provide
+from mediatr import Mediator, GenericQuery
+from pydantic import BaseModel
+
+from src.adapters.uow import AbstractUoW
+from src.di_container import Container
+from src.domain.services import LoadDocumentService
+
+
+class DocAddModel(BaseModel, GenericQuery[...]):
+    vec_col_name: str
+    doc_processing_cnf_id: int
+    files: dict[str, bytes]
+
+
+@Mediator.handler
+class DocAddHandler:
+    @inject
+    def __init__(
+            self,
+            doc_proc_cnf_uow: AbstractUoW = Provide[Container.doc_proc_cnf_uow],
+            load_document_service: LoadDocumentService = Provide[Container.load_document_service]
+    ):
+        self.doc_proc_cnf_uow = doc_proc_cnf_uow
+        self.load_document_service = load_document_service
+
+    async def handle(self, request: DocAddModel) -> ...:
+        with self.doc_proc_cnf_uow as uow:
+            doc_proc_cnf = uow.repository.read(request.doc_processing_cnf_id)
+
+        loaded_docs = await self.load_document_service.load_async(
+            files=request.files
+        )
+
+        print(loaded_docs)
