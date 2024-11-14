9965aa2b13896a56f5f40c0970bdb0efd4e79049
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2024-11-14 19:59:40 +0200

    added api key ccredential add page on ui

--- scribe_ui/src/app/api-key/add/page.tsx
+++ scribe_ui/src/app/api-key/add/page.tsx
@@ -1,3 +1,112 @@
+'use client';
+
+import {
+    Box,
+    Toolbar,
+    Typography,
+    Divider,
+    TextField,
+    Button,
+    Snackbar
+} from '@mui/material';
+import {useState} from 'react';
+import {useRouter} from 'next/navigation';
+
+import {API_URL} from "@/src/config";
+
 export default function Page() {
-    return <h1>add</h1>
+    const router = useRouter();
+    const [name, setName] = useState('');
+    const [apiKey, setApiKey] = useState('');
+    const [openSnackbar, setOpenSnackbar] = useState(false);
+    const [snackbarMessage, setSnackbarMessage] = useState('');
+
+    async function handleSubmit() {
+        if (!name || !apiKey) {
+            setSnackbarMessage("fulfill both fields 😡");
+            setOpenSnackbar(true);
+            return;
+        }
+
+        try {
+            const response = await fetch(
+                `${API_URL}/api-key/`,
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',  // Sending JSON data
+                        'accept': 'application/json',         // Telling the server we expect JSON response
+                    },
+                    body: JSON.stringify(
+                        {
+                            name: name,
+                            api_key: apiKey
+                        }
+                    )
+                }
+            );
+            if (response.status == 201) {
+                router.push('/api-key');
+            } else {
+                setSnackbarMessage(`smth went wrong 😢, status code: ${response.status}`);
+                setOpenSnackbar(true);
+            }
+        } catch (error) {
+            setSnackbarMessage(`smth went wrong 😢, error: ${error.message}`);
+            setOpenSnackbar(true);
+        }
+    }
+
+    return (
+        <Box
+            display={'flex'}
+            flexDirection={"column"}
+            alignItems={'flex-start'}
+            gap={2}
+        >
+            <Toolbar>
+                <Typography variant={'h6'}>
+                    Add Api Key Credential
+                </Typography>
+            </Toolbar>
+            <Divider sx={{width: '100%'}}/>
+
+            <Box display={"flex"} gap={2}>
+                <TextField
+                    id={'name'}
+                    variant={'standard'}
+                    label={'name'}
+                    value={name}
+                    onChange={(e => {
+                        setName(e.target.value)
+                    })}
+                />
+
+                <TextField
+                    id={'api-key'}
+                    variant={'standard'}
+                    label={'api key'}
+                    value={apiKey}
+                    onChange={(e) => {
+                        setApiKey(e.target.value)
+                    }}
+                />
+            </Box>
+
+            <Button
+                variant={'outlined'}
+                onClick={handleSubmit}
+            >
+                submit
+            </Button>
+
+            {/* informing snackbar*/}
+            <Snackbar
+                open={openSnackbar}
+                message={snackbarMessage}
+                onClose={() => setOpenSnackbar(false)}
+                autoHideDuration={3000}
+            />
+        </Box>
+    );
 }
