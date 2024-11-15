9dc2702293424b5e74f9c50ac8338dc3f3a3a2ae
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2024-11-15 15:31:31 +0200

    added sys-prompt add page

--- scribe_ui/src/app/sys-prompt/add/page.tsx
+++ scribe_ui/src/app/sys-prompt/add/page.tsx
@@ -1,3 +1,132 @@
-export default function Page() {
+'use client';
 
+import {
+    Box,
+    Typography,
+    Divider,
+    TextField,
+    Button,
+    Snackbar,
+    Breadcrumbs,
+    Link as MUILink
+} from '@mui/material';
+import {useState} from 'react';
+import {useRouter} from 'next/navigation';
+import Link from 'next/link';
+
+import {SysPromptPostModel} from '../models';
+import {API_URL} from "@/src/constants";
+
+export default function Page() {
+    const router = useRouter();
+    const [name, setName] = useState('');
+    const [content, setContent] = useState('');
+    const [openSnackbar, setOpenSnackbar] = useState(false);
+    const [snackbarMessage, setSnackbarMessage] = useState('');
+
+    async function handleSubmit() {
+        if (!name || !content) {
+            setSnackbarMessage("fulfill both fields 😡");
+            setOpenSnackbar(true);
+            return;
+        }
+
+        try {
+            const postRequest = SysPromptPostModel.parse({
+                name: name,
+                content: content
+            })
+
+            const response = await fetch(
+                `${API_URL}/sys-prompt/`,
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(postRequest)
+                }
+            );
+            if (response.status == 201) {
+                router.push('/sys-prompt');
+            } else {
+                setSnackbarMessage(`smth went wrong 😢, status code: ${response.status}`);
+                setOpenSnackbar(true);
+            }
+        } catch (error) {
+            setSnackbarMessage(`smth went wrong 😢, error: ${error.message}`);
+            setOpenSnackbar(true);
+        }
+    }
+
+    return (
+        <Box
+            display={'flex'}
+            flexDirection={"column"}
+            alignItems={'flex-start'}
+            gap={2}
+        >
+            {/*TOP PANEL*/}
+            <Breadcrumbs>
+                <Typography variant={'h6'}>
+                    <MUILink
+                        component={Link}
+                        href={'/sys-prompt'}
+                        underline={'none'}
+                    >
+                        sys-prompt
+                    </MUILink>
+                </Typography>
+                <Typography variant={'h6'}>
+                    add
+                </Typography>
+            </Breadcrumbs>
+
+            <Divider sx={{width: '100%'}}/>
+
+            {/* CONTENT */}
+            <Box
+                display={"flex"}
+                flexDirection={'column'}
+                gap={2}
+                width={'50%'}
+            >
+                {/* NAME */}
+                <TextField
+                    id={'name'}
+                    variant={'standard'}
+                    label={'name'}
+                    value={name}
+                    sx={{width: '50%'}}
+                    onChange={(e => {setName(e.target.value)})}
+                />
+
+                {/* CONTENT */}
+                <TextField
+                    id={'sys-prompt'}
+                    label={'sys-prompt'}
+                    multiline
+                    rows={16}
+                    variant={'outlined'}
+                    onChange={(e) => {setContent(e.target.value)}}
+                />
+
+            </Box>
+
+            <Button
+                variant={'outlined'}
+                onClick={handleSubmit}
+            >
+                submit
+            </Button>
+
+            {/* INFO SNACKBAR */}
+            <Snackbar
+                open={openSnackbar}
+                message={snackbarMessage}
+                onClose={() => setOpenSnackbar(false)}
+                autoHideDuration={3000}
+            />
+        </Box>
+    );
 }
--- scribe_ui/src/constants.ts
+++ scribe_ui/src/constants.ts
@@ -1,2 +1,2 @@
 export const API_URL: string  = 'http://localhost:8000';
-export const PAGE_LIMIT: number = 4;
+export const PAGE_LIMIT: number = 2;
