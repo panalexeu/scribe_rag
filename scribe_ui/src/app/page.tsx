348d293e5d9f10bcfb643b1073306dcf9a9ce773
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2024-11-17 13:44:37 +0200

    updated main page in scribe ui

--- scribe_ui/src/app/page.tsx
+++ scribe_ui/src/app/page.tsx
@@ -1,5 +1,23 @@
+import {Box, Breadcrumbs, Divider, IconButton, Tooltip, Typography} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+
 export default function Page() {
     return (
-        <p>Scribe</p>
+        <Box
+            display={'flex'}
+            flexDirection={"column"}
+            alignItems={'flex-start'}
+            gap={2}
+        >
+            <Box display={'flex'} width={'100%'}>
+                <Breadcrumbs>
+                    <Typography variant={'h6'}>
+                        scribe
+                    </Typography>
+                </Breadcrumbs>
+            </Box>
+
+            <Divider sx={{width: '100%'}}/>
+        </Box>
     );
 }
