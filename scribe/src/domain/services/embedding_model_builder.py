c5b8cf1b760efc5f055fea0e6caf4426a468fa60
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2025-05-04 17:07:28 +0300

    implemented cuda support

--- scribe/src/domain/services/embedding_model_builder.py
+++ scribe/src/domain/services/embedding_model_builder.py
@@ -19,7 +19,6 @@ class EmbeddingModelBuilder:
     def build(
             self,
             embedding_model: EmbeddingModel,
-            device: Literal['cpu', 'cuda'] = 'cuda'
     ) -> embedding_functions.EmbeddingFunction:
         """
         Decodes API-key and forms embedding function.
@@ -33,19 +32,19 @@ class EmbeddingModelBuilder:
                     return embedding_functions.OpenAIEmbeddingFunction(
                         api_key=decoded_api_key,
                         model_name=embedding_model.name.value,
-                        device=device
+                        device=embedding_model.device.value
                     )
                 case ModelProvider.COHERE:
                     return embedding_functions.CohereEmbeddingFunction(
                         api_key=decoded_api_key,
                         model_name=embedding_model.name.value,
-                        device=device
+                        device=embedding_model.device.value
                     )
 
         else:
             return embedding_functions.SentenceTransformerEmbeddingFunction(
                 model_name=embedding_model.name.value,
-                device=device
+                device=embedding_model.device.value
             )
 
     @staticmethod
